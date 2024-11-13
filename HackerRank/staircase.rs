3d4c45850ffab4a73415ddd7c70b8fdc8d0858f3
XDHARD <ya.gicevuch@e-u.edu.ua>
2024-11-13 20:50:15 +0200

    staircase commit

---
+++ HackerRank/staircase.rs
@@ -0,0 +1,24 @@
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fn staircase(n: i32) {
+    for i in 1..=n {
+        let space = " ".repeat((n - i) as usize);
+        let hashe = "#".repeat(i as usize);
+        println!("{}{}", space, hashe);
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    staircase(n);
+}
