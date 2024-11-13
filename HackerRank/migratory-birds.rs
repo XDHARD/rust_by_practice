7ef67055cf67ebe9a9db808ece66bf9c3f937d2b
XDHARD <ya.gicevuch@e-u.edu.ua>
2024-11-14 00:03:16 +0200

    migratory birds commit

---
+++ HackerRank/migratory-birds.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fn migratoryBirds(arr: &[i32]) -> i32 {
+    let mut counts = [0; 5];
+    for &bird in arr {
+        counts[(bird - 1) as usize] += 1;
+    }
+    let mut max_count = 0;
+    let mut result = 1;
+    for (i, &count) in counts.iter().enumerate() {
+        if count > max_count || (count == max_count && i + 1 < result) {
+            max_count = count;
+            result = i + 1;
+        }
+    }
+    result as i32
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _arr_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let arr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = migratoryBirds(&arr);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
